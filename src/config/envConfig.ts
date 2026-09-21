06fc69fee77581a3c86c2452ad915cab76a9f077
Dibbo <divyajitchakraborty@gmail.com>
2026-09-22 00:02:58 +0600

    chore: migrate to bun and configure project for Vercel deployment

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,9 +1,11 @@
-import dotenv from "dotenv"
-import path from "path"
-
-dotenv.config({
-    path: path.join(process.cwd(), ".env")
-})
+// import dotenv from "dotenv"
+// import path from "path"
+// 
+// if (process.env.NODE_ENV !== "production") {
+//     dotenv.config({
+//         path: path.join(process.cwd(), ".env")
+//     })
+// }
 
 export default {
     app_url: process.env.APP_URL,
